6607d2e33abb5f362229d658dd1a35466abf6564
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 01:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function nowServing(katzDeli) {
 
 function currentLine(line) {
   var i = 0;
-  if (katzDeli.length === 0) {
+  if (line.length === 0) {
     return "The line is currently empty.";
   }
   else {
     return "The line is currently: "
   }
   while (i < line.length) {
-    return `${i+1} ${katzDeli[i]}` 
+    return `${i+1} ${line[i]}` 
     i++;
   }
 }
