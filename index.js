c5b1f1e363ed438e8776cdb7d0722bc72eb5c480
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:42:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,3 @@ function currentLine(line) {
     return 'The line is currently empty.'
   }
 }
-
-  1) deli currentLine(line) says who is in line when there are people waiting:
-
-      Error: Expected 'The line is currently: Bill,Jane,Ann' to equal 'The lineis currently: 1. Bill, 2. Jane, 3. Ann'
-      + expected - actual
-
-      -The line is currently: Bill,Jane,Ann
-      +The line is currently: 1. Bill, 2. Jane, 3. Ann
