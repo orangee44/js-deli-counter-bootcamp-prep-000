115fa4a4713f9a2f2253c8186b8e6d8b6b41ef52
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:39:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(katzDeli) {
 
 function currentLine(line) {
   if (line.length > 0) {
+    for (i=0; )
     return `The line is currently: ${line}`
   }
   else {
@@ -21,3 +22,10 @@ function currentLine(line) {
   }
 }
 
+  1) deli currentLine(line) says who is in line when there are people waiting:
+
+      Error: Expected 'The line is currently: Bill,Jane,Ann' to equal 'The lineis currently: 1. Bill, 2. Jane, 3. Ann'
+      + expected - actual
+
+      -The line is currently: Bill,Jane,Ann
+      +The line is currently: 1. Bill, 2. Jane, 3. Ann
