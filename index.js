e347e4462172b16936dfa997afdfb2344eb5d709
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:28:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function nowServing(katzDeli) {
   }
 }
 
+  1) deli nowServing returns an announcement about the person it is serving, and shifts the line:
+
+      Error: Expected [ 'Steven', 'Blake', 'Avi' ] to equal [ 'Blake', 'Avi' ]
+      + expected - actual
+
+       [
+      -  "Steven"
+         "Blake"
+         "Avi"
+       ]
+
+      at assert (node_modules/expect/lib/assert.js:29:9)
+      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
+      at Context.it (test/index-test.js:40:24)
