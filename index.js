9467ca0de2c29ee26198bb9aab48f1fb09b6b95f
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ function takeANumber(katzDeli, name) {
 
 function nowServing(katzDeli) {
   if (katzDeli.length>0) {
-    return `Currently serving ${katzDeliLine[0]}.`
+    return `Currently serving ${katzDeli[0]}.`
   }
   else {
     return 'There is nobody waiting to be served!'
   }
 }
+
+
+  1) deli nowServing returns an announcement about the person it is serving, and shifts the line:
+     ReferenceError: katzDeliLine is not defined
+      at nowServing (file://index.html:10:35)
+      at Context.it (test/index-test.js:39:14)
