0b0a961a45b70a79fc8b4c03fbdae1b891617d3c
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
   if (line.length > 0) {
     return `The line is currently: `
     for (var i=0; i<line.length; i++) {
-      return `${i+1}. ${line} `
+      return `${i+1}. ${line[i]} `
     }
   }
   else {
