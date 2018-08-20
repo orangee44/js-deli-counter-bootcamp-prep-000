f8c11f92dc009beb58f7142723b30f93c899ecf3
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function takeANumber(katzDeli, name) {
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
+function nowServing() {
+  
+}
+
+
+  1) deli nowServing returns the line is empty when no one is on line:
+     ReferenceError: nowServing is not defined
+      at Context.it (test/index-test.js:34:7)
+
+  2) deli nowServing returns an announcement about the person it is serving, and shifts the line:
+     ReferenceError: nowServing is not defined
+      at Context.it (test/index-test.js:39:7)
