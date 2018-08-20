e0a2416d9177c533a4af74f715bada5d1964cbac
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function takeANumber(katzDeli, name) {
 
 function nowServing(katzDeli) {
   if (katzDeli.length>0) {
-    return `Currently serving ${katzDeli[0]}.`
-    katzDeli.shift();
-    return katzDeli;
+    return `Currently serving ${katzDeli.shift()}.`
   }
   else {
     return 'There is nobody waiting to be served!'
