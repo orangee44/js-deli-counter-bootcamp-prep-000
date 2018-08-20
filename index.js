519acc26d9c0d3166d480a76cb97da5978dbbc13
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,18 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(line) {
+  var i = 0;
   
+}
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
