ac8a4d79401bf8ee6abbcfa3ad89445381d2d167
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(line) {
-  return `The line is currently: ${line}`
+  if (line.length > 0) {
+    return `The line is currently: ${line}`
+  }
+  else {
+    return 'The line is curretly empty.'
+  }
 }
+
+
+  1) deli currentLine(line) returns "The line is currently empty." if no one isin line:
+
+      Error: Expected 'The line is currently: ' to equal 'The line is currentlyempty.'
+      + expected - actual
+
+      -The line is currently:
+      +The line is currently empty.
+
+      at assert (node_modules/expect/lib/assert.js:29:9)
+      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
+      at Context.it (test/index-test.js:46:31)
