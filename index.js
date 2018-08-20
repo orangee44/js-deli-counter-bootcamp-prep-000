35ae7d4c5841353b58c718e86e48ffc91145a021
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ function takeANumber(katzDeli, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine) {
+  if (katzDeliLine.length>0) {
     return `Currently serving ${katzDeliLine[0]}.`
   }
   else {
     return 'There is nobody waiting to be served!'
   }
 }
-
-  1) deli nowServing returns the line is empty when no one is on line:
-
-      Error: Expected 'Currently serving undefined.' to equal 'There is nobody waiting to be served!'
-      + expected - actual
-
-      -Currently serving undefined.
-      +There is nobody waiting to be served!
-
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.it (test/index-test.js:34:30)
