83bc3f5880aadd039100adf9f19efbea7a2fd14e
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-20 00:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,24 +6,9 @@ function takeANumber(katzDeli, name) {
 function nowServing(katzDeli) {
   if (katzDeli.length>0) {
     return `Currently serving ${katzDeli[0]}.`
-    
+    katzDeli.shift();
   }
   else {
     return 'There is nobody waiting to be served!'
   }
 }
-
-  1) deli nowServing returns an announcement about the person it is serving, and shifts the line:
-
-      Error: Expected [ 'Steven', 'Blake', 'Avi' ] to equal [ 'Blake', 'Avi' ]
-      + expected - actual
-
-       [
-      -  "Steven"
-         "Blake"
-         "Avi"
-       ]
-
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.it (test/index-test.js:40:24)
